8fef3a6f581c6596d58bd7cf24318bda4efe42d9
ErnAbr <ern_abr@yahoo.com>
2025-01-13 21:43:34 +0200

    using context instead of prop drilling

---
+++ Performant Context API/src/App.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import Sidebar from "./components/sidebar";
+import Main from "./components/main";
+import NavController from "./context/nav-controller";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+export default function App() {
+  return (
+    <NavController>
+      <Container>
+        <Sidebar />
+        <Main />
+      </Container>
+    </NavController>
+  );
+}
